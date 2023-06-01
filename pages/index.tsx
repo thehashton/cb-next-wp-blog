44bdbe051099c8db2244a3cfdc6e6d1a3482d8aa
Harry Ashton <harry.ashton.uk@gmail.com>
2023-06-01 22:42:04 +0100

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,26 @@ import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
 
+// Get the first 20 posts from WordPress, ordered by the date
+export async function getAllPostsFromWordPress(preview) {
+    const data = await fetch(`
+    query AllPosts {
+      posts(first: 20, where: { orderby: { field: DATE, order: DESC } }) {
+        edges {
+          node {
+            title
+            excerpt
+            slug
+            date
+          }
+        }
+      }
+    }
+  `)
+
+    return data.posts
+}
+
 export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node
   const morePosts = edges.slice(1)
@@ -15,7 +35,7 @@ export default function Index({ allPosts: { edges }, preview }) {
   return (
     <Layout preview={preview}>
       <Head>
-        <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+        <title>{`Curious Byte NextJS ${CMS_NAME} Blog`}</title>
       </Head>
       <Container>
         <Intro />
@@ -29,6 +49,7 @@ export default function Index({ allPosts: { edges }, preview }) {
             excerpt={heroPost.excerpt}
           />
         )}
+          <h1 style={{color: 'red'}}>{}</h1>
         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
       </Container>
     </Layout>
@@ -43,3 +64,4 @@ export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
     revalidate: 10,
   }
 }
+
